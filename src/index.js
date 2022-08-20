82bd070ace088d1c76ca5ac2fdd2f1221ad3a85d
Violetta <viol.fedoseenko@yandex.ru>
2022-08-21 00:03:48 +0300

    add app structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore} from '@reduxjs/toolkit'
+import { Provider } from 'react-redux';
+import { rootReducer } from './redux/reducers/rootReducer';
+
+
+const store = configureStore ( 
+  {reducer: {rootReducer}},
+
+  window.__REDUX_DEVTOOLS_EXTENTION__ && window.__REDUX_DEVTOOLS_EXTENTION__()
+
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
